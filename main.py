cc8637d79d6705ede37c3e5752e24d6d2b105166
hope-karthi <94035417+hope-karthi@users.noreply.github.com>
2022-02-02 01:23:04 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from starlette.config import Config
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
+
+from authlib.integrations.starlette_client import OAuth
+app = FastAPI(docs_url=None, redoc_url=None)
+app.add_middleware(SessionMiddleware, secret_key='!secret')
+
+@app.get('/')
+async def home(request: Request):
+    user = request.session.get('user')
+    if user is not None:
+        email = user['email']
+        html = (
+            f'<pre>Email: {email}</pre><br>'
+            '<a href="/docs">documentation</a><br>'
+            '<a href="/logout">logout</a>'
+        )
+        return HTMLResponse(html)
+    return HTMLResponse('<a href="/login">login</a>')
+
+# Initialize our OAuth instance from the client ID and client secret specified in our .env file
+config = Config('.env')
+oauth = OAuth(config)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
+
+@app.get('/login', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def login(request: Request):
+    # Redirect Google OAuth back to our application
+    redirect_uri = request.url_for('auth')
